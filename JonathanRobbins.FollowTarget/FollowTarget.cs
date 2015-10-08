5640cff20a416eed01295543d10d76c020daff91
islaytitans <jonathan.m.robbins@gmail.com>
2015-10-08 14:00:05 +0100

    Dev / Added first command

---
+++ JonathanRobbins.FollowTarget/FollowTarget.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Shell.Framework.Commands;
+
+namespace JonathanRobbins.FollowTarget
+{
+    public class FollowTarget : Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
