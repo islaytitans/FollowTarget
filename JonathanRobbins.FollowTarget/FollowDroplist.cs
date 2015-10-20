bd31d572348ffa6e32841cf054f654dc59687686
islaytitans <jonathan.m.robbins@gmail.com>
2015-10-20 14:21:00 +0100

    Dev / Added for droplist

---
+++ JonathanRobbins.FollowTarget/FollowDroplist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Shell.Framework.Commands;
+using Sitecore.Web;
+
+namespace JonathanRobbins.FollowTarget
+{
+    public class FollowDroplist : Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            var targetId = WebUtil.GetFormValue(context.Parameters["fieldId"]);
+
+            Sitecore.Context.ClientPage.SendMessage(this, "item:load(id=" + targetId + ")");
+        }
+    }
+}
