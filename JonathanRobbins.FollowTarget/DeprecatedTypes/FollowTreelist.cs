0aeadb214e5ed0e9489f4b0a54a05eafc60a566d
islaytitans <jonathan.m.robbins@gmail.com>
2016-07-28 13:57:02 +0100

    Dev / addition of the deprecated type Treelist which is still used for BaseTemplate field

---
+++ JonathanRobbins.FollowTarget/DeprecatedTypes/FollowTreelist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Shell.Framework.Commands;
+
+namespace JonathanRobbins.FollowTarget.DeprecatedTypes
+{
+    public class FollowTreelist : ListTypes.FollowTreelist
+    {
+
+    }
+}
